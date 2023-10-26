bdf57e660c5833365b06b70467571067d9d41621
John Savin <johnsavin@proton.me>
2023-10-26 10:11:02 -0400

    build simple counter in react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
+import { useState } from 'react';     // Hook(start with word use and used at top level of component) that is used regularly
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0)
+  //must use square brackets
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='App-header'>
+        <h1>Counter</h1>
+        <h2>You clicked {counter} times.</h2>
+        <div>
+          <button onClick={() => setCounter(counter + 1)}>+</button>
+          <button onClick={() => setCounter(0)}>Reset</button>
+          <button onClick={() => setCounter(counter > 0 ? counter - 1 : 0)}>-</button>  
+        
+        </div>
       </header>
     </div>
   );
